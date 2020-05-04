b7bb08b9557582813fc088ee6b60412fc4b74755
AnnaSemibabnova <61233581+AnnaSemibabnova@users.noreply.github.com>
2020-05-04 17:03:54 +0300

    lab

---
+++ lab_vector/vector/vector.h
@@ -0,0 +1,43 @@
+﻿#include "matrix.h"
+
+template <class T1>
+class Vector :public Matrix<T1>
+{
+public:
+  Vector() :Matrix() {}
+  Vector(int _size) :Matrix(1, _size) {}
+  Vector(const Vector& t) :Matrix(t) {}
+  ~Vector() {}
+  T1* get_vector();
+  
+  using Matrix:: operator =;
+  
+  T1& operator [](const int i);
+  
+  void set_length(int _resize);
+  
+  template<class J>
+  friend Vector<J> operator /(Vector<J>& lhs, Vector<J>& rhs);
+  
+  template<class Y>
+  friend Y operator *(Vector<Y>& lhs, Vector<Y>& rhs);
+  
+  template<class K>
+  friend istream& operator >> (istream& in, Vector<K>& rhs);
+  
+  friend void In_Vector(Vector& lhs)
+  {
+    for (int i = 0; i < lhs.get_length(); i++)
+      lhs[i] = rand();
+  }
+  
+  template<class P>
+  friend clock_t BubbleSort(Vector<P>& B);
+  
+  template<class Q>
+  friend clock_t InsertionSort(Vector<Q>& b);
+  
+  template<class K>
+  friend clock_t QuickSort(Vector<K>& p, int first, int last);
+};
+#include "vector_proceed.h"
